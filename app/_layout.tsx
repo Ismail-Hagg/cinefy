c87d53796e2f66c44d744306c20ea75b008acf2c
Ismail-Hagg <seemoe06@gmail.com>
2024-09-03 20:59:56 +0300

    added firebase

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,12 +10,11 @@ import { useEffect } from "react";
 import "react-native-reanimated";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
